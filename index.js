2b8313bcb02d69851ba8debfce1277f401d2d80b
Greg Allen <greg@firstandthird.com>
2016-11-23 12:18:14 -0800

    switched to tag based and log if bypass used

--- index.js
+++ index.js
@@ -5,28 +5,34 @@ const defaultOptions = {
   // the date the embargo will be lifted:
   embargoEnd: null,
   embargoResponse: 'Page Unavailable',
-  viewsOnly: true
-  // routes with these tags could be included, and leave bank to do all routes:
-  // tags: []
+  tag: 'embargo'
 };
 
 exports.register = (server, config, next) => {
   const options = defaults(config, defaultOptions);
   if (!options.embargoEnd) {
-    return next('You must specify a time for the ebargo to end by passing the embargoEnd parameter');
+    return next('You must specify a time for the embargo to end by passing the embargoEnd parameter');
   }
   options.embargoEndTime = moment(options.embargoEnd);
   // log time that the embargo will be lifted:
   server.log(['hapi-embargo', 'info'], `Embargo will be lifted at ${options.embargoEndTime}, time is now ${moment().local()}`);
   server.ext({
-    type: 'onRequest',
+    type: 'onPreAuth',
     method: (request, reply) => {
       // if bypass option matches then let them pass:
       if (options.bypass && request.query.bypass === options.bypass) {
+        server.log(['hapi-embargo', 'info'], {
+          message: 'Embargo bypassed',
+          url: request.url.path,
+          routePath: request.route.path,
+          referrer: request.info.referrer,
+          ipAddress: request.info.remoteAddress,
+          userAgent: request.headers['user-agent']
+        });
         return reply.continue();
       }
-      // if it's not a view then let them pass:
-      if (options.viewsOnly && !reply.render) {
+
+      if (!request.route.settings.tags || request.route.settings.tags.indexOf(options.tag) === -1) {
         return reply.continue();
       }
       // otherwise see if the embargo is expired
